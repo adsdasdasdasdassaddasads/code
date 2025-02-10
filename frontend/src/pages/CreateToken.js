b3ebb193bac9da0e6646bb061c40d3d9b87d17d5
adsdasdasdasdassaddasads <tesad9552@gmail.com>
2025-02-10 13:03:03 +0000

    Initial setup of React frontend with routing and components

---
+++ frontend/src/pages/CreateToken.js
@@ -0,0 +1,13 @@
+// src/pages/CreateToken.js
+import React from 'react';
+import TokenCreation from '../components/TokenCreation';
+
+const CreateToken = () => {
+  return (
+    <div className="create-token">
+      <TokenCreation />
+    </div>
+  );
+};
+
+export default CreateToken;
